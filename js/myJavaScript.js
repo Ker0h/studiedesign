8c35bb16163d0d64fd1ed0f6271dc0dc2144874e
Stijn van Veen <stijnboz@live.nl>
2017-10-08 23:38:05 +0200

    added quiz page

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,3 +1,31 @@
-function myfunction(){
+var points = 0;
 
+function makeActiveA1(number){
+    var awnser1 = document.getElementById('q1a1');
+    var awnser2 = document.getElementById('q1a2');
+    if(number === 1){
+        if(awnser2.className === "list-group-item active") {
+            awnser2.className = "list-group-item list-group-item-action";
+            awnser1.className = "list-group-item active";
+            points++;
+        }if(awnser1.className === "list-group-item active"){
+
+        } else {
+            awnser1.className = "list-group-item active";
+            points++;
+        }
+    }
+    if(number === 2){
+        if(awnser1.className === "list-group-item active"){
+            awnser1.className = "list-group-item list-group-item-action";
+            awnser2.className = "list-group-item active";
+            points--;
+        }if(awnser2.className === "list-group-item active"){
+
+        }else{
+            awnser2.className = "list-group-item active";
+            points--;
+        }
+    }
+    console.log(points);
 }
