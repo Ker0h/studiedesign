a9d59b3512b4f63a7c1dc8a56dea723b8e30cbe0
Stijn van Veen <stijnboz@live.nl>
2017-10-15 17:02:28 +0200

    fix spelling mistake

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,6 +1,9 @@
 var points = 0;
 var questionsAnswered = [];
 
+//Deze functie kijkt allereerst naar welk antwoord is aangeklikt en controlleerd of een ander antwoord al eerder was aangeklikt.
+//Daarbij voegt hij de vraag toe aan een array en geeft punten voor de beantwoorde vraag.
+//Als de array alle vragen bevat verwijdert hij de vragen en print hij je uitslag uit.
 function makeActive(questionNumber, answerNumber){
     var answer1 = document.getElementById("q" + questionNumber + "a1");
     var answer2 = document.getElementById("q" + questionNumber + "a2");
@@ -40,11 +43,11 @@ function makeActive(questionNumber, answerNumber){
             finalscoreElement.innerHTML = '<div id="finalScore57" class="finalScore"><h1>Resultaat 5-7</h1><p>Je interesse is zeker terug te zien uit de score van deze quiz. De opleiding Informatica zou je zeker in je opleidingskeuze moeten meenemen!</p><a href="index.html"><button type="button" class="btn btn-primary">Terug naar de Homepage</button></a></div>'
         }
         if (points < 5){
-            finalscoreElement.innerHTML = '<div id="finalScore15" class="finalScore"><h1>Resultaat 1-5</h1><p>hmmm, misschien is Informatica niet helemaal de opleiding die je zoekt. Gelukkig biedt avans nog vele andere\nopleidingen waar je interesses misschien beter aansluiten!</p><a href="index.html"><button type="button" class="btn btn-primary">Terug naar de Homepage</button></a></div>'
+            finalscoreElement.innerHTML = '<div id="finalScore15" class="finalScore"><h1>Resultaat 1-5</h1><p>Hmmm, misschien is Informatica niet helemaal de opleiding die jij zoekt. Gelukkig biedt Avans nog vele andere\nopleidingen waar je interesses misschien beter aansluiten!</p><a href="index.html"><button type="button" class="btn btn-primary">Terug naar de Homepage</button></a></div>'
         }
     }
 }
-
+//Deze funtie kijkt naar of de vraag al eerder was beantwoord, zo niet voeg de vraag toe aan de array en geef per punten voor. zo wel doe niks.
 function checkIfAlreadyCounted(questionNumber, answer){
     for (var i = 0; i < questionsAnswered.length; i++){
         if(questionsAnswered[i] === questionNumber){
